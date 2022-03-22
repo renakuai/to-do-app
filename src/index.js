4ce2ef21b41958a5147fddb4a5110ad09494672e
Rena <rena.kuai@gmail.com>
2022-03-21 19:51:02 -0500

    wired up all states, errors, and main functionality

--- src/index.js
+++ src/index.js
@@ -1,26 +1,46 @@
 import _ from 'lodash';
 import './style.css';
-import {createTodo, updateTodoDOM, storeAndGroup, createList, updateListDOM} from './todolist.js';
+import {createTodo, updateTodoDOM, storeAndGroup, createList, updateListDOM, modifyTodo, taskMatch, todoArray} from './todolist.js';
 
 document.getElementById("duedate").valueAsDate = new Date();
 
 //open task modal
 const createTaskBtn = document.querySelector('.create-task-btn');
-createTaskBtn.addEventListener('click', () => {
+createTaskBtn.addEventListener('click', (e) => {
+  e.stopPropagation();
+  updateListDOM.closeListModal();
   updateTodoDOM.openTaskModal();
 })
 
 //open list modal
 const createListBtn = document.querySelector('.create-list-btn');
-createListBtn.addEventListener('click', () => {
+createListBtn.addEventListener('click', (e) => {
+  e.stopPropagation();
+  updateTodoDOM.closeTaskModal();
   updateListDOM.openListModal();
 })
 
+//validating forms
+const form = document.querySelectorAll('input');
+form.forEach((input => {
+  input.addEventListener('input', (e) => {
+    e.stopPropagation();
+    input.setCustomValidity('');
+    input.checkValidity();
+    console.log('error');
+  });
+  input.addEventListener('invalid', (e) => {
+    e.stopPropagation();
+    if (input.value === '') {
+      input.setCustomValidity('This is a required input');
+    };
+  })
+}))
 
 //creating todo item
-let i = 1;
+let i = 0;
 const addBtn = document.getElementById('add-btn');
-addBtn.addEventListener('click', () => {
+addBtn.addEventListener('click', (e) => {
   let calDate = document.getElementById('duedate').value;
   let convDate = storeAndGroup.convertDate(calDate);
   const todo = createTodo(
@@ -29,26 +49,31 @@ addBtn.addEventListener('click', () => {
     convDate,
     document.querySelector('input[name="priority"]:checked').value,
     document.querySelector('.main-title').textContent, 
-    i++);
+    i++,
+    'incomplete');
   storeAndGroup.storeTodo(todo);
   updateTodoDOM.displayTodo(todo);
   updateTodoDOM.closeTaskModal();
+  e.stopPropagation();
 })
 
 //creating a list
+let listCount = 100;
 const addListBtn = document.getElementById('add-list-btn');
 addListBtn.addEventListener('click', (e) => {
   const list = createList(
     document.getElementById("list-name").value, 
     document.getElementById("list-desc").value, 
-    i++);
+    ++listCount);
   let clickedList = list;
   updateListDOM.displaySideList(list);
   updateListDOM.highlightList(clickedList);
   updateListDOM.clearPage();
   updateListDOM.setTitle(clickedList);
   updateListDOM.closeListModal();
+  storeAndGroup.showGroupedList(clickedList);
   createTaskBtn.style.display = "block";
+  e.stopPropagation();
 })
 
 //switching by list
@@ -62,6 +87,7 @@ listLinks.forEach((link) => {
     storeAndGroup.groupTodoProject(clickedList);
     storeAndGroup.showGroupedList(clickedList);
     createTaskBtn.style.display = "block";
+    e.stopPropagation();
   });
 })
 
@@ -73,8 +99,10 @@ dateLinks.forEach((link) => {
     updateListDOM.clearPage();
     updateListDOM.setTitle(clickedList);
     storeAndGroup.groupTodoOther(clickedList)
+    updateListDOM.highlightList(clickedList);
     storeAndGroup.showGroupedList(clickedList);
     createTaskBtn.style.display = "none";
+    e.stopPropagation();
   });
 })
 
@@ -90,22 +118,155 @@ window.addEventListener('load', (e) => {
   updateListDOM.highlightList(list);
   updateListDOM.setTitle(clickedList);
   const todo = createTodo(
-    'This is a task!', 
-    'You can even write notes', 
-    '3/18/2022',
+    'This is your first task!', 
+    'You can even add notes to help you with your tasks!', 
+    '03/18/2022',
     'High',
     document.querySelector('.main-title').textContent, 
-    i++);
+    0,
+    'incomplete');
+    console.log(todoArray);
   storeAndGroup.storeTodo(todo);
   updateTodoDOM.displayTodo(todo);
 });
 
-//expanding todo
+
+//actions on each todo row
+let rowId = [];
 const parent = document.querySelectorAll('.main-body');
-parent.forEach((row) => {
-  row.addEventListener('click', (e) => {
+parent.forEach((item) => {
+  item.addEventListener('click', (e) => {
+    e.stopPropagation();
+     //expanding todo
     if (e.target.classList == 'chevron') {
       updateTodoDOM.expandTodo(e);
     }
+    //expand actions modal
+    else if (e.target.classList == 'overflow') {
+      if (document.querySelector('.action-modal') !== null) {
+        updateTodoDOM.closeActionModal(e);
+      }
+      updateTodoDOM.openActionModal(e);
+    }
+    else if (e.target.classList == 'action-edit') {
+      let tgtId = e.target.parentNode.parentNode.parentNode.parentNode.id;
+      rowId.push(tgtId);
+      modifyTodo.findTodoPosition(e);
+      modifyTodo.findTodo(e);
+      updateTodoDOM.openEditTaskModal();
+      modifyTodo.displayInFormDOM(e);
+      updateTodoDOM.closeActionModal(e);
+    }
+    else if (e.target.classList == 'action-delete') {
+      let tgtId = e.target.parentNode.parentNode.parentNode.parentNode.id;
+      rowId.push(tgtId);
+      modifyTodo.findTodoPosition(e);
+      modifyTodo.findTodo(e);
+      modifyTodo.removeNonModified(e);
+      document.getElementById(rowId[0]).parentNode.remove();
+      rowId = [];
+    }
+    else if (e.target.classList == 'incomplete') {
+      rowId = [];
+      let tgtId = e.target.parentNode.parentNode.parentNode.id;
+      rowId.push(tgtId);
+      modifyTodo.findForComplete(e);
+      updateTodoDOM.check(e);
+      let clickedList = document.querySelector('.main-title').textContent;
+      const todo = createTodo(
+        taskMatch[0].title, 
+        taskMatch[0].notes, 
+        taskMatch[0].duedate,
+        taskMatch[0].priority,
+        document.querySelector('.main-title').textContent, 
+        rowId[0],
+        'complete');
+      document.querySelector('.main-body').textContent ='';
+      modifyTodo.removeReplaceComplete(todo);
+      storeAndGroup.groupTodoProject(clickedList);
+      storeAndGroup.showGroupedList(clickedList);
+      e.stopPropagation();
+    }
+    else if (e.target.classList == 'complete') {
+      rowId = [];
+      let tgtId = e.target.parentNode.parentNode.parentNode.id;
+      rowId.push(tgtId);
+      modifyTodo.findForComplete(e);
+      updateTodoDOM.uncheck(e);
+      let clickedList = document.querySelector('.main-title').textContent;
+      const todo = createTodo(
+        taskMatch[0].title, 
+        taskMatch[0].notes, 
+        taskMatch[0].duedate,
+        taskMatch[0].priority,
+        document.querySelector('.main-title').textContent, 
+        rowId[0],
+        'incomplete');
+      document.querySelector('.main-body').textContent ='';
+      modifyTodo.removeReplaceComplete(todo);
+      storeAndGroup.groupTodoProject(clickedList);
+      storeAndGroup.showGroupedList(clickedList);
+      e.stopPropagation();
+    }
   })
 })
+
+
+const editBtn = document.getElementById('edit-task-btn');
+editBtn.addEventListener('click', (e) => {
+  let calDate = document.getElementById('edit-duedate').value;
+  console.log(calDate);
+  let convDate = storeAndGroup.convertDate(calDate);
+  let clickedList = document.querySelector('.main-title').textContent;
+  const todo = createTodo(
+    document.getElementById("edit-title").value, 
+    document.getElementById("edit-notes").value, 
+    convDate,
+    document.querySelector('input[name="edit-priority"]:checked').value,
+    document.querySelector('.main-title').textContent, 
+    rowId[0],
+    'incomplete');
+  document.querySelector('.main-body').textContent ='';
+  modifyTodo.removeNonModified(e);
+  modifyTodo.replaceTodo(todo);
+  storeAndGroup.groupTodoProject(clickedList);
+  storeAndGroup.showGroupedList(clickedList);
+  updateTodoDOM.closeEditTaskModal();
+  console.log(todoArray);
+  e.stopPropagation();
+})
+
+//closing modals + popovers when not clicked
+const taskModal = document.querySelector('.create-task-modal');
+const listModal = document.querySelector('.create-list-modal');
+const editTaskModal = document.querySelector('.edit-task-modal');
+
+const body = document.querySelector('body');
+window.addEventListener('click', (e) => {
+  if (e.target == body){
+    return;
+  }
+  else if (taskModal.style.display == 'block' && (!taskModal.contains(e.target) || e.target.classList == 'close')) {
+    console.log('if fired');
+    updateTodoDOM.closeTaskModal();
+    e.stopPropagation();
+  }
+  else if (editTaskModal.style.display == 'block' && (!editTaskModal.contains(e.target) || e.target.classList == 'close')) {
+    console.log('if fired');
+    updateTodoDOM.closeEditTaskModal();
+    e.stopPropagation();
+  }
+  else if (listModal.style.display == 'block' && (!listModal.contains(e.target) ||  e.target.classList == 'close')) {
+    console.log('if fired');
+    updateListDOM.closeListModal();
+    e.stopPropagation();
+  }
+  else if (document.querySelector('.action-modal') !== null && !document.querySelector('.action-modal').contains(e.target)) {
+    updateTodoDOM.closeActionModal();
+  }
+})
+
+
+
+
+
--- src/todolist.js
+++ src/todolist.js
@@ -1,11 +1,15 @@
 import CheckBlank from './img/check-blank.svg';
 import CheckFilled from './img/check-filled.svg';
 import ChevronUp from './img/chevron-up.svg';
+import Logo from './img/todolist.svg';
 import ChevronDown from './img/chevron-down.svg';
-
+import Overflow from './img/overflow.svg';
+import Trash from './img/delete.svg';
+import Pencil from './img/pencil.svg';
+import Llama from './img/llama.svg';
 
 //factory function for new to do
-const createTodo = (title, notes, duedate, priority, project, id) => {
+const createTodo = (title, notes, duedate, priority, project, id, status) => {
   return {
     title: title,
     notes: notes,
@@ -13,21 +17,67 @@ const createTodo = (title, notes, duedate, priority, project, id) => {
     priority: priority,
     project: project,
     id: id,
+    status: status,
     //project: document.getElementById("project").value,
   }
 }
 
 //module for modifying todo
+let taskMatch = [];
+let taskPos = [];
 const modifyTodo = (() => {
-  function changePriority() {
-    todoObj.priority = document.getElementById("title").value;
+  function findTodo(e) {
+    taskMatch = [];
+    let editArray = todoArray.filter(todo => todo.id == e.target.parentNode.parentNode.parentNode.parentNode.id);
+    let editTodo = editArray[0];
+    taskMatch.push(editTodo);
   }
-  function markStatus() {
-    //clicked = finished
+  function findTodoPosition(e) {
+    taskPos = [];
+    let arrayPos = todoArray.findIndex(todo => todo.id == e.target.parentNode.parentNode.parentNode.parentNode.id);
+    taskPos.push(arrayPos); // return position in array
+  }
+  function displayInFormDOM(e) {
+    document.getElementById("edit-title").value 
+    = taskMatch[0].title;
+    document.getElementById("edit-notes").value = taskMatch[0].notes;
+    let date = taskMatch[0].duedate;
+    let convDate = date[6] + date[7] + date[8] + date[9] + '-' + date[0] + date[1] + '-' + date[3] + date[4];
+    console.log(convDate);
+    document.getElementById("edit-duedate").value = convDate;
+    document.querySelector('input[name="priority"]:checked').value = taskMatch[0].priority;
+  }
+  function removeNonModified(e) {
+    console.log(taskPos[0]);
+    let position = taskPos[0];
+    todoArray.splice(position, 1);
+  }
+  function replaceTodo(todo) {
+    let position = taskPos[0];
+    todoArray.splice(position, 0, todo);
+  }
+  function findForComplete(e) {
+    taskMatch = [];
+    let editArray = todoArray.filter(todo => todo.id == e.target.parentNode.parentNode.parentNode.id);
+    let editTodo = editArray[0];
+    taskMatch.push(editTodo); //find object in the array
+    taskPos = [];
+    let arrayPos = todoArray.findIndex(todo => todo.id == e.target.parentNode.parentNode.parentNode.id);
+    taskPos.push(arrayPos); // return position in array
+  }
+  function removeReplaceComplete(todo) {
+    let position = taskPos[0];
+    todoArray.splice(position, 1); // remove the original obj from array
+    todoArray.splice(position, 0, todo); // replace with the new obj stemming from todo
   }
   return {
-    //changePriority,
-    //markStatus,
+    findTodo : findTodo,
+    findTodoPosition : findTodoPosition,
+    removeNonModified : removeNonModified,
+    replaceTodo : replaceTodo,
+    displayInFormDOM : displayInFormDOM,
+    findForComplete : findForComplete,
+    removeReplaceComplete : removeReplaceComplete,
   }
 })();
 
@@ -38,6 +88,34 @@ const updateTodoDOM = (() => {
   }
   function openTaskModal() {
     document.querySelector('.create-task-modal').style.display = 'block';
+  }
+  function closeEditTaskModal() {
+    document.querySelector('.edit-task-modal').style.display = 'none';
+    console.log(todoArray);
+  }
+  function openEditTaskModal() {
+    document.querySelector('.edit-task-modal').style.display = 'block';
+  }
+  function check(e) {
+    let parent = e.target.parentNode;
+    let sibling = e.target.parentNode.nextSibling;
+    e.target.remove();
+    const checked = new Image();
+    checked.src = CheckFilled;
+    checked.classList.add('complete');
+    sibling.classList.add('crossed');
+    parent.appendChild(checked);
+  }
+  function uncheck(e) {
+    console.log(e.target.parentNode);
+    let parent = e.target.parentNode;
+    let sibling = e.target.parentNode.nextSibling;
+    sibling.classList.remove('crossed');
+    e.target.remove();
+    const check = new Image();
+    check.src = CheckBlank;
+    check.classList.add('incomplete');
+    parent.appendChild(check);
   }
   function displayTodo(todo, clickedList) {
     const main = document.querySelector('.main-body');
@@ -53,23 +131,38 @@ const updateTodoDOM = (() => {
       row.appendChild(row__left);
         const row__status = document.createElement('div');
         row__status.classList.add('todo-row__status');
+        if (todo.status == 'complete') {
+          const checkfilled = new Image();
+          checkfilled.src = CheckFilled;
+          checkfilled.classList.add('complete');
+          row__status.appendChild(checkfilled);
+        }
+        else if (todo.status == 'incomplete') {
           const checkbox = new Image();
           checkbox.src = CheckBlank;
+          checkbox.classList.add('incomplete');
           row__status.appendChild(checkbox);
+        }
         row__left.appendChild(row__status);
-        const row__title = document.createElement('div');
-        row__title.classList.add('todo-row__title');
-        row__title.textContent = todo.title;
-        row__left.appendChild(row__title);
+        const row__titleproject = document.createElement('div');
+        row__titleproject.classList.add('todo-row__titleproject');
+        row__left.appendChild(row__titleproject);
+          const row__title = document.createElement('div');
+          row__title.classList.add('todo-row__title');
+          if (todo.status == 'complete') {
+            row__title.classList.add('crossed');
+          }
+          row__title.textContent = todo.title;
+          row__titleproject.appendChild(row__title);
+          if (clickedList == "all" || clickedList == "today" || clickedList == "week") {
+            const row__project = document.createElement('div');
+            row__project.classList.add('todo-row__projectName');
+            row__project.textContent = todo.project;
+            row__titleproject.appendChild(row__project);
+          }
       const row__right = document.createElement('div');
       row__right.classList.add('todo-row__right');
       row.appendChild(row__right);
-      if (clickedList == "all" || clickedList == "today" || clickedList == "week") {
-        const row__project = document.createElement('div');
-        row__project.classList.add('todo-row__projectName');
-        row__project.textContent = todo.project;
-        row__right.appendChild(row__project);
-      }
         const row__priority = document.createElement('div');
         if (todo.priority == 'Low') {
           row__priority.classList.add('todo-row__low');
@@ -95,6 +188,13 @@ const updateTodoDOM = (() => {
           chevronDown.classList.add('chevron');
           row__chevron.appendChild(chevronDown);
           row__right.appendChild(row__chevron);
+        const row__overflow = document.createElement('div');
+        row__overflow.classList.add('overflow');
+            const overflow = new Image();
+            overflow.src = Overflow;
+            overflow.classList.add('overflow');
+            row__overflow.appendChild(overflow);
+            row__right.appendChild(row__overflow);
   }
   function expandTodo(e) {
     if (e.target.parentNode.parentNode.parentNode.parentNode.children[1] !== undefined) {
@@ -111,6 +211,13 @@ const updateTodoDOM = (() => {
       let fullRow = e.target.parentNode.parentNode.parentNode.parentNode;
       const expanded = document.createElement('div');
       expanded.classList.add('expanded');
+      fullRow.appendChild(expanded);
+      const notesLabel = document.createElement('div');
+      notesLabel.classList.add('notes-label');
+      notesLabel.textContent = 'Notes';
+      expanded.appendChild(notesLabel);
+      const notesText = document.createElement('div');
+      notesText.classList.add('notes-text');
       let retrieved = todoArray.filter(note => note.id == todoRowId);
       let parent = e.target.parentNode;
       e.target.remove();
@@ -118,15 +225,51 @@ const updateTodoDOM = (() => {
         chevronUp.src = ChevronUp;
         chevronUp.classList.add('chevron');
         parent.appendChild(chevronUp);
-      expanded.textContent = retrieved[0].notes;
-      fullRow.appendChild(expanded);
+      notesText.textContent = retrieved[0].notes;
+      expanded.appendChild(notesText);
     }
+  }
+  function openActionModal(e) {
+    let overflowDiv = e.target.parentNode;
+    const actionModal = document.createElement('div');
+    actionModal.classList.add('action-modal');
+    const edit = document.createElement('div');
+    edit.classList.add('action-edit');
+    const editimg = document.createElement('div');
+    edit.appendChild(editimg);
+      editimg.classList.add('pencil');
+      const pencil = new Image();
+      pencil.src = Pencil;
+      editimg.appendChild(pencil);
+    edit.textContent = 'Edit task';
+    actionModal.appendChild(edit);
+    const del = document.createElement('div');
+    del.classList.add('action-delete');
+    const delimg = document.createElement('div');
+    del.appendChild(delimg);
+    delimg.classList.add('trashcan');
+      const trash = new Image();
+      trash.src = Trash;
+      delimg.appendChild(trash);
+    del.textContent = 'Delete task';
+    actionModal.appendChild(del);
+    overflowDiv.appendChild(actionModal);
+  }
+  function closeActionModal(e) {
+    const actionModal = document.querySelector('.action-modal')
+    actionModal.remove();
   }
   return {
     closeTaskModal : closeTaskModal,
     openTaskModal : openTaskModal,
     displayTodo: displayTodo,
     expandTodo : expandTodo,
+    openActionModal : openActionModal,
+    closeActionModal : closeActionModal,
+    openEditTaskModal : openEditTaskModal,
+    closeEditTaskModal : closeEditTaskModal,
+    check : check,
+    uncheck : uncheck,
   }
 })();
 
@@ -179,6 +322,20 @@ const storeAndGroup = (() => {
     }
   }
   function showGroupedList(clickedList) {
+    if (matchingArray.length == 0) {
+      const main = document.querySelector('.main-body');
+      const empty = document.createElement('div');
+      empty.classList.add('empty-state');
+      const emptyText = document.createElement('div');
+      emptyText.classList.add('empty-text');
+      emptyText.textContent = 'Lucky you! There are no tasks here.';
+      const llama = new Image();
+          llama.src = Llama;
+          llama.classList.add('llama');
+      empty.appendChild(llama);
+      empty.appendChild(emptyText);
+      main.appendChild(empty);     
+    }
     for (let i=0; i < matchingArray.length; i++) {
       let todo = matchingArray[i];
       updateTodoDOM.displayTodo(todo, clickedList);
@@ -281,4 +438,7 @@ export {
   createTodo,
   updateTodoDOM,
   storeAndGroup,
+  modifyTodo,
+  taskMatch,
+  todoArray,
 }
